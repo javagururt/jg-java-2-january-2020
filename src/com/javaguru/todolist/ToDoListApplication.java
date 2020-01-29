0a7b150475ea772fd86526059c349f326cffe1cc
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2020-01-29 17:53:40 +0200

    #lesson2 SRP

---
+++ src/com/javaguru/todolist/ToDoListApplication.java
@@ -0,0 +1,11 @@
+package com.javaguru.todolist;
+
+import com.javaguru.todolist.console.ConsoleUI;
+
+public class ToDoListApplication {
+
+    public static void main(String[] args) {
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.execute();
+    }
+}
